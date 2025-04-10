a575c5b18b47f3f9a1070e262f167434a4eea429
Borja Luengo <borja.luengoprieto@affirm.com>
2025-04-10 16:23:28 +0200

    wip

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/bluengop/rabbitmq-client
 go 1.23.2
 
 require github.com/michaelklishin/rabbit-hole/v3 v3.2.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	rabbithole "github.com/michaelklishin/rabbit-hole/v3"
 )
 
@@ -12,23 +15,50 @@ RabbitHole: 	https://pkg.go.dev/github.com/michaelklishin/rabbit-hole/v3
 				https://github.com/michaelklishin/rabbit-hole
 */
 
-func CreateRabbitMQClient(endpoint, user, pass string) *rabbithole.Client {
+func main() {
+	// Load credentials
+	err := godotenv.Load("secrets.env")
+	if err != nil {
+		panic("Error loading secrets.env file")
+	}
+	endpoint := os.Getenv("RMQ_ENDPOINT")
+	user := os.Getenv("RMQ_USER")
+	pass := os.Getenv("RMQ_PASS")
+
+	rmqc, err := createRabbitMQClient(endpoint, user, pass)
+	if err != nil {
+		log.Fatalf("Unable to create RabbitMQ client: %s\n", err)
+		panic(err)
+	}
+
+	queues, err := getRabbitMQqueues(rmqc)
+	if err != nil {
+		log.Fatalf("Unable to create RabbitMQ client: %s\n", err)
+		panic(err)
+	}
+	for _, queue := range *queues {
+		log.Printf("Queue: %s, Node: %s\n", queue.Name, queue.Node)
+	}
+	log.Printf("Finished.")
+}
+
+func createRabbitMQClient(endpoint, user, pass string) (*rabbithole.Client, error) {
 	fmt.Println("Creating RabbitMQ client")
 	client, err := rabbithole.NewClient(endpoint, user, pass)
 	if err != nil {
-		fmt.Printf("Error creating RabbitMQ client: %s\n", err)
-		panic(err)
+		log.Fatalf("Error creating RabbitMQ client: %s\n", err)
+		return nil, err
 	}
-	return client
+	return client, nil
 }
 
-func GetRabbitMQqueues(client *rabbithole.Client, queueName string) {
+func getRabbitMQqueues(client *rabbithole.Client) (*[]rabbithole.QueueInfo, error) {
 	fmt.Println("Getting RabbitMQ queues")
-	queues, err := client.GetQueues()
+	var queues []rabbithole.QueueInfo
+	queues, err := client.ListQueues()
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error when listing queues in the vhost: %s\n", err)
+		return nil, err
 	}
-	for _, queue := range queues {
-		fmt.Printf("Queue: %s, State: %d\n", queue.Name, queue.State)
-	}
+	return &queues, nil
 }
